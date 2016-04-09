038e350ac3df855d1e361d28cd0b620bf6ce69c7
Arjan van de Ven <arjan@linux.intel.com>
2016-04-09 22:41:37 +0000

    fs: ext4: fsync: optimize double-fsync() a bunch
    There are cases where EXT4 is a bit too conservative sending barriers down to
    the disk; there are cases where the transaction in progress is not the one
    that sent the barrier (in other words: the fsync is for a file for which the
    IO happened more time ago and all data was already sent to the disk).
    
    For that case, a more performing tradeoff can be made on SSD devices (which
    have the ability to flush their dram caches in a hurry on a power fail event)
    where the barrier gets sent to the disk, but we don't need to wait for the
    barrier to complete. Any consecutive IO will block on the barrier correctly.
    
    Signed-off-by: Raphiel Rollerscaperers <rapherion@raphielgang.org>
    Signed-off-by: Bcap03 <60796531+Bcap03@users.noreply.github.com>

--- include/linux/blkdev.h
+++ include/linux/blkdev.h
@@ -1368,6 +1368,8 @@ extern int blkdev_issue_write_same(struct block_device *bdev, sector_t sector,
 
 #define BLKDEV_DISCARD_SECURE	(1 << 0)	/* issue a secure erase */
 
+extern int blkdev_issue_flush(struct block_device *, gfp_t, sector_t *);
+extern void blkdev_issue_flush_nowait(struct block_device *, gfp_t);
 extern int blkdev_issue_discard(struct block_device *bdev, sector_t sector,
 		sector_t nr_sects, gfp_t gfp_mask, unsigned long flags);
 extern int __blkdev_issue_discard(struct block_device *bdev, sector_t sector,
@@ -2099,6 +2101,10 @@ static inline int blkdev_issue_flush(struct block_device *bdev, gfp_t gfp_mask,
 				     sector_t *error_sector)
 {
 	return 0;
+}
+
+static inline void blkdev_issue_flush_nowait(struct block_device *bdev, gfp_t gfp_mask)
+{
 }
 
 #endif /* CONFIG_BLOCK */
